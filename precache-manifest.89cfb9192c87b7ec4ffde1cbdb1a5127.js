008dda3f717d3ae99c6e3ff3e0643c064287d60d
Radoslaw Czerniawski <radoslaw.czerniawski@o2.pl>
2021-11-16 17:45:57 +0100

    First attempt at deploying an app

---
+++ precache-manifest.89cfb9192c87b7ec4ffde1cbdb1a5127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fc2deadbe9c41e9be4b",
+    "url": "/static/css/main.27e90025.chunk.css"
+  },
+  {
+    "revision": "4fc2deadbe9c41e9be4b",
+    "url": "/static/js/main.4fc2dead.chunk.js"
+  },
+  {
+    "revision": "3cd95d8456d266dd7dad",
+    "url": "/static/js/1.3cd95d84.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/static/media/logo.a87d0f35.svg"
+  },
+  {
+    "revision": "c454cc1f224087631005e13cf6a3aece",
+    "url": "/index.html"
+  }
+];
