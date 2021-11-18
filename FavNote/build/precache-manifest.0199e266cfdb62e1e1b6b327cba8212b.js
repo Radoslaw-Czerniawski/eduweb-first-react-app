275f2963b95c045d79c7ab144ccccda7a7b510d8
Radoslaw Czerniawski <radoslaw.czerniawski@o2.pl>
2021-11-18 16:20:41 +0100

    again changed build's directory

---
+++ FavNote/build/precache-manifest.0199e266cfdb62e1e1b6b327cba8212b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "affff4c01e92e33d2075",
+    "url": "/static/css/main.c626fd5c.chunk.css"
+  },
+  {
+    "revision": "affff4c01e92e33d2075",
+    "url": "/static/js/main.affff4c0.chunk.js"
+  },
+  {
+    "revision": "3cd95d8456d266dd7dad",
+    "url": "/static/js/1.3cd95d84.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/static/media/logo.a87d0f35.svg"
+  },
+  {
+    "revision": "7c741cd511e832faa88fae4b5be1a08f",
+    "url": "/index.html"
+  }
+];
