aced47099e1a0dec19c4e613be7d502f1740e038
Radoslaw Czerniawski <radoslaw.czerniawski@o2.pl>
2021-11-18 16:55:25 +0100

    another deploy test

---
+++ build/precache-manifest.b2be18cd335826285a20b23f558890a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6a7e9edd833d46b93a5",
+    "url": "/static/css/main.ad6f7aed.chunk.css"
+  },
+  {
+    "revision": "f6a7e9edd833d46b93a5",
+    "url": "/static/js/main.f6a7e9ed.chunk.js"
+  },
+  {
+    "revision": "3cd95d8456d266dd7dad",
+    "url": "/static/js/1.3cd95d84.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/static/media/logo.a87d0f35.svg"
+  },
+  {
+    "revision": "c5ca0b817124d0126a40df3eac1d7958",
+    "url": "/index.html"
+  }
+];
